62e3ff416ea01d5a6a9dcfc1c8122c049b79d429
merjunemong <kdjun016@naver.com>
2024-10-09 15:08:24 +0900

    modified:   config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.164.184.249']
 
 APPEND_SLASH = False
 
