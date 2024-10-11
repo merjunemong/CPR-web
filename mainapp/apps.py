565f3f730b5c8c75e4ed5e768f9a9c80c74e374e
merjunemong <kdjun016@naver.com>
2024-10-12 00:16:06 +0900

    modified:   mainapp/apps.py

--- mainapp/apps.py
+++ mainapp/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.apps import AppConfig
-from mainapp.views import dump_neo4j_db
+from mainapp.views import backup_data
 
 class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -10,5 +10,5 @@ class MainappConfig(AppConfig):
     def ready(self):
         scheduler = BackgroundScheduler()
         # 매일 자정마다 DB 덤프 실행
-        scheduler.add_job(dump_neo4j_db, 'cron', hour=0, minute=0)
+        scheduler.add_job(backup_data, 'cron', hour=0, minute=0)
         scheduler.start()
