15cd821704fba7a353c1570287933b5d6fca0dc4
merjunemong <kdjun016@naver.com>
2024-10-29 02:54:44 +0900

    modified:   mainapp/functions/CPR_GraphDB.py

--- mainapp/functions/CPR_GraphDB.py
+++ mainapp/functions/CPR_GraphDB.py
@@ -88,17 +88,10 @@ class CPR_GraphDB:
     
     def add_data(self, job_name, new_skill):
         query = (
-<<<<<<< HEAD
             "MATCH (keco:KECO {name: $job_name}) "
             "MERGE (ncs:NCS {name: $new_skill}) "
             "MERGE (keco)-[:HAS_JOB]->(ncs) "
             "MERGE (ncs)-[:HAS_JOB]->(keco) "
-=======
-            "MATCH (keco:KECO {name: $job_name}) " # 직업 찾기
-            "MERGE (ncs:NCS {name: $new_skill}) " # 스킬 없으면 추가
-            "MERGE (keco)-[:HAS_JOB]->(ncs) " # 양방향 관계 추가
-            "MERGE (ncs)-[:HAS_JOB]->(keco) " # 양방향 관계 추가
->>>>>>> da04ec5da043c670e7bc02d9ba3355458893a2b2
         )
         parameters={"job_name": job_name, "new_skill": new_skill}
         
@@ -133,11 +126,7 @@ class CPR_GraphDB:
 def openApp():
     scheme = "neo4j"  # Connecting to Aura, use the "neo4j+s" URI scheme
     host_name = "13.209.36.125"
-<<<<<<< HEAD
     # host_name = "localhost"
-=======
-    #host_name = "localhost"
->>>>>>> da04ec5da043c670e7bc02d9ba3355458893a2b2
     port = 7687
     uri = f"{scheme}://{host_name}:{port}"
     user = "neo4j"
